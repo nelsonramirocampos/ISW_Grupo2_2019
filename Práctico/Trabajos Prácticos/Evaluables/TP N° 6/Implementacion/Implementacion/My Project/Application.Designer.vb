36bf87f76be55abc8fe1715c3ca4e256e0695644
marcosarce04 <marcosarce04@gmail.com>
2019-10-17 01:42:17 -0300

    Revert "Merge branch 'master' of https://github.com/nelsonramirocampos/ISW_Grupo2_2019"
    This reverts commit 1cb5f25b3cbc9d83c8723ac4766c41f7b923fec7, reversing
    changes made to f5afaabed01d3acd8cd92ba34f218ba722f98e7c.

--- Práctico/Trabajos Prácticos/Evaluables/TP N° 6/Implementacion/Implementacion/My Project/Application.Designer.vb
+++ Práctico/Trabajos Prácticos/Evaluables/TP N° 6/Implementacion/Implementacion/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Implementacion.frmPrincipal
+            Me.MainForm = Global.Implementacion.Principal
         End Sub
     End Class
 End Namespace
